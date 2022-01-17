ccf6454bc08771335871a6a6ab463b1a512f1067
Mateus Macedo <mhbassomacedo@gmail.com>
2022-01-17 08:36:39 -0300

    Using command flutter create . to rebuild the project and made modifications
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+            onPressed: () => {shuffleImage()},
+            child: Image.asset('images/ball$ballNumber.png'),
+          ))
+        ],
+      ),
+    );
+  }
+
+  void shuffleImage()
+  {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print("I got clicked");
+    });
+  }
+}
